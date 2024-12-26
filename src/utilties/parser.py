5648e1fa50e22ac3950692872215f33da47986f1
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-12-26 06:06:28 +0300

    added tron wallet parser

---
+++ src/utilties/parser.py
@@ -0,0 +1,17 @@
+from tronpy import Tron
+
+from schemas.wallet import WalletRequestCreate
+
+
+async def get_request(adress: str) -> WalletRequestCreate:
+    tron_client = Tron()
+    try:
+        account_info = tron_client.get_account(addr=adress)
+        account = WalletRequestCreate(
+            bandwith=account_info["bandwith"],
+            balance=account_info["balance"] / 100000,
+            energy=account_info["energy"],
+        )
+        return account
+    except Exception as e:
+        raise Exception(str(e))
